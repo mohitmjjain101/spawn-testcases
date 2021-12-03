0b7c6db16bab305a24552efffc964c4da6ae1df1
mohitmjjain101 <mohitmjjain101@gmail.com>
2021-12-03 18:33:30 +0530

    Update sum.test.js

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -26,28 +26,28 @@
 // });
 
 
-var mockSpawn = require('mock-spawn');
-import { spawn } from 'child_process';
-var init = require('./../sum')
-var mySpawn = mockSpawn();
-require('child_process').spawn = mySpawn;
+// var mockSpawn = require('mock-spawn');
+// import { spawn } from 'child_process';
+// var init = require('./../sum')
+// var mySpawn = mockSpawn();
+// require('child_process').spawn = mySpawn;
 
-mySpawn.setDefault(mySpawn.simple(1 /* exit code */, 'hello world' /* stdout */));
-mySpawn.sequence.add(mySpawn.simple(0));
-mySpawn.sequence.add(mySpawn.simple(1));
-mySpawn.sequence.add(function (cb) {
-    setTimeout(function () { return cb(2); }, 2000);
-});
-mySpawn.sequence.add(function (cb) {
-    // test the error handling of your library
-    this.emit('error', new Error('spawn ENOENT'));
-    setTimeout(function() { return cb(8); }, 10);
-});
-mySpawn.sequence.add({throws:new Error('spawn ENOENT')});
-describe('test', function () {
-  test('actual test ', function(){
-   let data =  init();
-   console.log(data)
-   expect(data);
-  })
-})
+// mySpawn.setDefault(mySpawn.simple(1 /* exit code */, 'hello world' /* stdout */));
+// mySpawn.sequence.add(mySpawn.simple(0));
+// mySpawn.sequence.add(mySpawn.simple(1));
+// mySpawn.sequence.add(function (cb) {
+//     setTimeout(function () { return cb(2); }, 2000);
+// });
+// mySpawn.sequence.add(function (cb) {
+//     // test the error handling of your library
+//     this.emit('error', new Error('spawn ENOENT'));
+//     setTimeout(function() { return cb(8); }, 10);
+// });
+// mySpawn.sequence.add({throws:new Error('spawn ENOENT')});
+// describe('test', function () {
+//   test('actual test ', function(){
+//    let data =  init();
+//    console.log(data)
+//    expect(data);
+//   })
+// })
